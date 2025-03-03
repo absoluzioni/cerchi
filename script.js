b631bf060b17c29a4338a6a66bb79c6c45b68bc2
absoluzioni <absoluzioni@gmail.com>
2025-03-03 12:37:05 +0100

    Aggiornate tutte le funzionalità per farle funzionae come da XD

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
-const circonferenza = document.querySelectorAll("[data-logo]");
+const q = document.querySelector(".disegno--q");
+const menus = document.querySelectorAll(".disegno__menu");
+const grafica = document.querySelector(".grafica");
+const firstVoice = document.querySelector(".disegno__menu--first");
+const pulsante = document.querySelector(".pulsante");
 
-circonferenza.forEach((circonferenza) => {
-  circonferenza.addEventListener("click", () => {
-    circonferenza.classList.toggle("active");
+q.addEventListener("mouseenter", (e) => visualizzaMenu(e));
+q.addEventListener("touchstart", (e) => visualizzaMenu(e));
+grafica.addEventListener("mouseleave", (e) => nascondiMenu(e));
+grafica.addEventListener("touchend", (e) => nascondiMenu(e));
+
+firstVoice.addEventListener("mouseenter", (e) => ruotaDisegno(e));
+firstVoice.addEventListener("touchstart", (e) => ruotaDisegno(e));
+firstVoice.addEventListener("mouseleave", (e) => ruotaDisegno(e));
+firstVoice.addEventListener("touchend", (e) => ruotaDisegno(e));
+
+setTimeout(() => {
+  pulsante.classList.remove("dn");
+}, 1500);
+
+function visualizzaMenu(e) {
+  let disegno = e.target;
+  const circonferenza = disegno.querySelector(".circonferenza");
+  const testo = disegno.querySelector(".circonferenza__testo");
+  const trapezio = disegno.querySelector(".trapezio");
+  const pulsante = disegno.querySelector(".pulsante");
+
+  circonferenza.classList.add("yellow-border");
+  trapezio.classList.add("yellow-bg");
+  pulsante.classList.add("dn");
+  testo.classList.remove("dn");
+
+  console.log(menus);
+
+  menus.forEach((menu) => {
+    menu.classList.remove("dn");
   });
-});
+}
+
+function nascondiMenu(e) {
+  let disegno = e.target;
+  const circonferenza = disegno.querySelector(".circonferenza");
+  const testo = disegno.querySelector(".circonferenza__testo");
+  const trapezio = disegno.querySelector(".trapezio");
+  const pulsante = disegno.querySelector(".pulsante");
+
+  circonferenza.classList.remove("yellow-border");
+  trapezio.classList.remove("yellow-bg");
+  pulsante.classList.remove("dn");
+  testo.classList.add("dn");
+
+  menus.forEach((menu) => {
+    menu.classList.add("dn");
+  });
+}
+
+function ruotaDisegno(e) {
+  let circonferenza = e.target.parentElement.querySelector(".circonferenza");
+  circonferenza.classList.toggle("active");
+}
 
